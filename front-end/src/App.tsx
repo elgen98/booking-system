73a9ce4b00602b9d74797c145907ff3f8babc693
Elliot Blomqvist <elliot.blomqvist@yahoo.com>
2022-08-18 10:49:49 +0200

    404 page added

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -5,6 +5,7 @@ import Booking from "./components/pages/Booking";
 import Contact from "./components/pages/Contact";
 import Home from "./components/pages/Home";
 import Layout from "./components/pages/Layout";
+import NoPage from "./components/pages/NoPage";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="booking" element={<Booking />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
