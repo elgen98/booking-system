0606e5260bde3c26769ac3d4ed777d77f7273970
Rasweb <Rasmuspalm@fastmail.se>
2022-08-18 10:54:57 +0200

    changed server link

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,8 +2,8 @@ import express, { Application, Request, Response } from "express";
 
 const app: Application = express();
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/server", (req: Request, res: Response) => {
   res.send("hello");
 });
 
-app.listen(8000, () => console.log("server http://localhost:8000"));
+app.listen(8000, () => console.log("server http://localhost:8000/server"));
