5ae5d83d7346b3dc7658a82703cd8c342552a88c
Rasweb <Rasmuspalm@fastmail.se>
2022-08-18 10:52:33 +0200

    Server setup

---
+++ back-end/src/app.ts
@@ -0,0 +1,9 @@
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("hello");
+});
+
+app.listen(8000, () => console.log("server http://localhost:8000"));
