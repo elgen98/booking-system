86be3056c0320946459d28d40d8baca171ac486d
Elliot Blomqvist <elliot.blomqvist@yahoo.com>
2022-08-19 14:10:30 +0200

    get booking

--- front-end/src/components/pages/Booking.tsx
+++ front-end/src/components/pages/Booking.tsx
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { useEffect } from "react";
-import BookingService from "../../services/BookingService";
 import DateAndGuest from "../DateAndGuest";
 
 export default function Booking() {
   useEffect(() => {
     axios.get("http://localhost:8000/bookings").then((response) => {
-      console.log(response.data);
+      console.log("data:", response.data);
     });
   }, []);
 
