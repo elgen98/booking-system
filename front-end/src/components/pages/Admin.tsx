bceb02a72f1e2e9f5a5809a0d8ba15f317222aaa
Rasweb <Rasmuspalm@fastmail.se>
2022-08-24 15:43:53 +0200

    Admin create bookings

--- front-end/src/components/pages/Admin.tsx
+++ front-end/src/components/pages/Admin.tsx
@@ -1,17 +1,28 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { HiX } from "react-icons/hi";
 
 interface IBookings {
   _id: String;
   name: String;
+  email: String;
+  telephone_number: String;
   guest_amount: Number;
-  time: Number;
+  time: String;
   date: String;
 }
 
 function Admin() {
   const [bookings, setBookings] = useState<IBookings[]>([]);
+  const [createBooking, setCreateBooking] = useState<IBookings>({
+    _id: "",
+    name: "",
+    email: "",
+    telephone_number: "",
+    guest_amount: 0,
+    time: "",
+    date: "",
+  });
 
   function removeBooking(e: any) {
     e.preventDefault();
@@ -29,6 +40,27 @@ function Admin() {
       .then((response) => setBookings(response.data));
   }, []);
 
+  // Add
+  function handleAdd(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.type === "number") {
+      setCreateBooking({ ...createBooking, [e.target.name]: +e.target.value });
+    } else {
+      setCreateBooking({ ...createBooking, [e.target.name]: e.target.value });
+    }
+  }
+
+  function handleAddSubmit(e: ChangeEvent<HTMLFormElement>) {
+    e.preventDefault();
+    axios
+      .post("http://localhost:8000/bookings/create", createBooking)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
   return (
     <div>
       <div className="flex flex-col mx-auto w-[85%]">
@@ -36,9 +68,11 @@ function Admin() {
           {bookings.map((booking, index) => (
             <div key={index} className="bg-slate-400 p-4 rounded-lg">
               <p>Bokad av: {booking.name}</p>
-              <p>Datum: {booking.date.split("T")[0]}</p>
-              <p>Klockan: {booking.time.toString()}</p>
-              <p>Antal gäster: {booking.guest_amount.toString()}</p>
+              <p>Email: {booking.email}</p>
+              <p>Telefon: {booking.telephone_number}</p>
+              <p>Datum {booking.date}</p>
+              <p>Klockan: {booking.time}</p>
+              {/* <p>Antal gäster: {booking.guest_amount}</p> */}
               <button
                 onClick={removeBooking}
                 id={booking._id.toString()}
@@ -56,6 +90,73 @@ function Admin() {
           ))}
         </div>
       </div>
+      <form onSubmit={handleAddSubmit}>
+        <div>
+          <label>
+            Namn
+            <input
+              type="text"
+              className="border-solid border-2 border-sky-500"
+              name="name"
+              onChange={handleAdd}
+            />
+          </label>
+
+          <label>
+            Email
+            <input
+              type="text"
+              className="border-solid border-2 border-sky-500"
+              name="email"
+              onChange={handleAdd}
+            />
+          </label>
+
+          <label>
+            Nummer
+            <input
+              type="text"
+              className="border-solid border-2 border-sky-500"
+              name="telephone_number"
+              onChange={handleAdd}
+            />
+          </label>
+        </div>
+
+        <div>
+          <label>
+            Gäster
+            <input
+              type="number"
+              className="border-solid border-2 border-sky-500"
+              name="guest_amount"
+              onChange={handleAdd}
+            />
+          </label>
+
+          <label>
+            Tid
+            <input
+              type="text"
+              className="border-solid border-2 border-sky-500"
+              name="time"
+              onChange={handleAdd}
+            />
+          </label>
+
+          <label>
+            Datum
+            <input
+              type="text"
+              className="border-solid border-2 border-sky-500"
+              name="date"
+              onChange={handleAdd}
+            />
+          </label>
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
