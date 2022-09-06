50dab7f534027c6e8c783c0ac2b005b3a04a4153
Rasweb <Rasmuspalm@fastmail.se>
2022-09-06 09:26:19 +0200

    Home page img styling

--- front-end/src/components/LandingText.tsx
+++ front-end/src/components/LandingText.tsx
@@ -3,7 +3,7 @@ export default function LandingText() {
     <section className=" flex flex-col items-center justify-center h-3/5">
       <p className=" underline decoration-solid">Bar och Restaurang</p>
       <img
-        className=" w-6/12"
+        className=" w-5/6 md:w-6/12"
         src="../assets/VuxenjuiceText.png"
         alt="Logotype"
       />
